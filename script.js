81450da0e82bb1cafdaa64c09a27f58a7fe161f7
Arminder Khinda <arminder.khinda@yahoo.com>
2020-10-14 15:10:14 -0700

    Add files

---
+++ script.js
@@ -0,0 +1,31 @@
+/* Arrow Functions */
+// let ask = (question, yes, no) => {
+//     confirm(question) ? yes() : no();
+// }
+
+// ask("Do you agree?", () => { alert("You agreed"); }, () => { alert("You canceled the execution"); });
+
+/* Objects */
+// let user = {
+//     name: "Arminder",
+//     age: 21,
+//     isAdmin: true
+// }
+// Object with const can be changed, but not as a whole
+// const user = {
+//     name: "Arminder"
+// }
+
+// user.name = "Frank"; // Can be done
+
+/* Object copying, references 
+    Primitives: Variables are assigned/copied as a whole value (Have two independent variables, each one stroing the string "Hello!")
+    let message = "Hello!";
+    let phrase = message
+
+    Object: A variable stores not the object, but the "address in memory" (reference) to it (Two variables, each one with the reference to the same object)
+    let user = { name: "John" };
+    let admin = user; // copy the reference
+*/
+
+
